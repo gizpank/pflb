0d44978ce5207ec819f6a69276c9ebda38087189
a.postnikov <a.postnikov@pflb.ru>
2017-12-18 17:54:07 +0400

    fix

--- test_gmail/src/test/java/com/pflb/postnikov/pages/LoginPage.java
+++ test_gmail/src/test/java/com/pflb/postnikov/pages/LoginPage.java
@@ -1,10 +1,7 @@
 package com.pflb.postnikov.pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage extends AbstractPage {
 
@@ -14,15 +11,8 @@ public class LoginPage extends AbstractPage {
     @FindBy(css = ".RveJvd")
     private WebElement loginNextButton;
 
-    @FindBy(xpath = "/html/body/div[1]/div[1]/div[2]/div[2]/form/div[2]/div/div[3]/div[1]/div[1]/div/div[1]/div/div[1]/input")
-    //@FindBy(css = "#password > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(1)")
+    @FindBy(name = "password")
     private WebElement passwordField;
-
-//    @FindBy(id = "login-button")
-//    private WebElement submitButton;
-//
-//    @FindBy(id = "login-failed")
-//    private WebElement logonInvalidMessage;
 
     private String BASE_URL = "http://mail.google.com";
 
